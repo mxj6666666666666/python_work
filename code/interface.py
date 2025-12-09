9dcc812b00d82bd16441729dd1b8c8e40a6ccd30
qimiaoshjie <1143674039@qq.com>
2025-12-09 14:53:33 +0800

    增加接口文件

---
+++ code/interface.py
@@ -0,0 +1,4 @@
+import window   # 窗口显示   要留一个文本框来输入方程
+import zero     # 显示原函数图像,并且计算函数有没有零点,有则输出零点,无则输出零
+import integral # 求函数积分 , 输出:函数的积分方程
+import derivative # 求函数导数 , 输出:函数的导数方程
---
+++ code/main.py
@@ -0,0 +1,8 @@
+import interface
+
+def main():
+    #这里要输入一个方程组字符串
+    pass
+
+if __name__ == "__main__":
+    main()
